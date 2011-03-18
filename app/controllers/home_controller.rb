91f7371cd196ef12583247b447eebe1759a09697
Vitaly Baum <butaji@Vitaly-Baums-MacBook-Pro.local>
2011-03-19 00:21:09 +0300

    created posts and info pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+ 
   def index
+  end
+  
+  def events
+  end
+  
+  def news
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Moscowaltnet::Application.routes.draw do
+  resources :posts
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -56,5 +58,5 @@ Moscowaltnet::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
