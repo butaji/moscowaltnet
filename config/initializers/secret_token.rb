edf5d36b31d00aca3bd03ebbfc77b2b4ee8f79aa
Vitaly Baum <butaji@Vitaly-Baums-MacBook-Pro.local>
2011-03-18 21:58:52 +0300

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moscowaltnet::Application.config.secret_token = 'e3b251067f0dabe78ba4ae7946ef73fa5ca9acb1cc5c242dfc48b4ec73d6a812afa299ce22477451d93c9ff75a7e81422dd9c5078dd3e2984004f412e8bc7153'
