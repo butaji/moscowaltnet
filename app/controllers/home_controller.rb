721f13a98499bd1d28ab66281a8661a57bd55195
Vitaly Baum <butaji@Vitaly-Baums-MacBook-Pro.local>
2011-03-19 11:49:23 +0300

    working on events anouncement from gcal atom feed to reduce dublicates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,15 @@
+require 'rubygems'
+require 'feed-normalizer'
+require 'open-uri'
+   
 class HomeController < ApplicationController
  
   def index
   end
   
   def events
+    @events = []
+    feed = FeedNormalizer::FeedNormalizer.parse open('http://www.iht.com/rss/frontpage.xml')
   end
   
   def news
