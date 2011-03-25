f97afbfe0a85a556f4f2f7096d5407caa23189a6
Vitaly Baum <butaji@Vitaly-Baums-MacBook-Pro.local>
2011-03-26 01:52:52 +0300

    added static events info and cache for that

--- Gemfile
+++ Gemfile
@@ -6,8 +6,8 @@ gem 'rails', '3.0.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', :require => 'sqlite3'
-gem 'feed-normalizer'
 gem 'jquery-rails', '>= 0.2.6'
+gem 'json'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
-require 'rubygems'
-require 'feed-normalizer'
 require 'open-uri'
+require 'json'
    
-class HomeController < ApplicationController
-  caches_action :events
- 
+class HomeController < ApplicationController 
   def index
   end
   
   def events
+    events_key = "events_data_key";
+    
+    if (!Rails.cache.read(events_key))
+      Rails.cache.write(events_key, JSON.parse(open("http://www.google.com/calendar/feeds/i1hvc1jnaki2jnrqept2i8sago%40group.calendar.google.com/public/full?futureevents=true&orderby=starttime&sortorder=ascending&max-results=1&alt=json").read) )
+    end
+    @events = Rails.cache.read(events_key)["feed"]["entry"][0]
   end
   
   def news
