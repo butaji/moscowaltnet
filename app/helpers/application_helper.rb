6f4f3f140d64c6d20456baeff5938dda7950a535
Vitaly Baum <vitaly.baum@gmail.com>
2011-03-26 14:22:58 +0300

    resolved issue with page_id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,14 @@ module ApplicationHelper
   def title(page_title)  
     content_for(:title) { page_title }  
   end
-
+  
+  def page_id()
+    action = params[:controller] + "_" + params[:action]
+    
+    if (params[:id])
+      action += "_" + params[:id]
+    end
+    
+    return action
+  end
 end
