6695c6bb183a010aade065ad956520a6d955f1e3
Vitaly Baum <butaji@Vitaly-Baums-MacBook-Pro.local>
2011-03-26 12:28:24 +0300

    a little changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,17 @@ require 'open-uri'
 require 'json'
    
 class HomeController < ApplicationController 
+  caches_action :lastevent, :expires_in => 24.hours, :layout => false
+  
   def index
   end
   
+  def lastevent
+    logger.debug "================ lastevent've called"
+    @lastevent = JSON.parse(open("http://www.google.com/calendar/feeds/i1hvc1jnaki2jnrqept2i8sago%40group.calendar.google.com/public/full?futureevents=true&orderby=starttime&sortorder=ascending&max-results=1&alt=json").read)["feed"]["entry"][0]
+  end
+  
   def events
-    events_key = "events_data_key";
-    
-    if (!Rails.cache.read(events_key))
-      Rails.cache.write(events_key, JSON.parse(open("http://www.google.com/calendar/feeds/i1hvc1jnaki2jnrqept2i8sago%40group.calendar.google.com/public/full?futureevents=true&orderby=starttime&sortorder=ascending&max-results=1&alt=json").read) )
-    end
-    @events = Rails.cache.read(events_key)["feed"]["entry"][0]
   end
   
   def news
