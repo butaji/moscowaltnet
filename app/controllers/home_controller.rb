6ff83bdf76dc07e0fab7e73201d4da0879960359
Vitaly Baum <vitaly.baum@gmail.com>
2011-04-02 03:20:49 +0400

    events on index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 require 'json'
    
 class HomeController < ApplicationController 
-  #caches_action :lastevent, :expires_in => 24.hours, :layout => false
-  #caches_action :index, :expires_in => 24.hours, :layout => false
+  caches_action :lastevent, :expires_in => 24.hours, :layout => false
+  caches_action :index, :expires_in => 24.hours, :layout => false
   
   def index
     @event = lastevent()
